2ca800b7940fd7c1f3e45f5c98dcdd3daecb722e
Akash Kumar Singh <akashsingh049811@gmail.com>
2021-06-17 10:12:27 +0530

    modified files

--- CCode/addition.c
+++ CCode/addition.c
@@ -8,3 +8,7 @@ int sum3 (int x, int y, int z) {
 	return (x+y+z);
 }
 
+int sum4 (int x, int y, int z, int t) {
+	return (x+y+z+t);
+}
+
--- JavaCode/Hello.java
+++ JavaCode/Hello.java
@@ -1,5 +1,8 @@
 class HelloWorld {
 	public static void main (String[] args) {
+		helloWorld ();
+	}
+	public void helloWorld () {
 		System.out.println ("Hello World!!");
 	}
 }
